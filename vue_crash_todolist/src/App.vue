dca7401971e2c502e78a983625623ff63074d141
ubervelocity <mihaitu.popescu@gmail.com>
2019-09-08 13:34:52 +0200

    init vue app

---
+++ vue_crash_todolist/src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="app">
+    <!-- To get the todo list in the template you use the v-bind directive -->
+    <h1>Todos</h1>
+  </div>
+</template>
+
+<script>
+import Todos from './components/Todos';
+
+export default {
+  name: 'app',
+  components: {
+    Todos
+  },
+  data() {
+    return {
+      todos: [
+        {
+          id: 1,
+          title: 'Learn Node.js',
+          completed: 'true'
+        },
+        {
+          id: 2,
+          title: 'Learn Express.js',
+          completed: 'true'
+        },
+        {
+          id: 3,
+          title: 'Learn Vue.js',
+          completed: 'false'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  line-height: 1.4;
+}
+</style>
---
+++ vue_crash_todolist/src/components/Todos.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>Todos</h1>
+    </div>    
+</template>
+
+<script>
+export default {
+    name: "Todos"
+}
+</script>
+
+<style scoped>
+
+</style>
