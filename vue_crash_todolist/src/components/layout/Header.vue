cfa965160377e1091099f24eba0f917369bef307
ubervelocity <mihaitu.popescu@gmail.com>
2019-09-08 15:26:04 +0200

    added contact page

--- vue_crash_todolist/src/components/layout/Header.vue
+++ vue_crash_todolist/src/components/layout/Header.vue
@@ -4,6 +4,7 @@
         <div id="nav">
             <router-link to="/">Home</router-link> |
             <router-link to="/about">About</router-link>
+            <router-link to="/contact">Contact</router-link>
         </div>
     </header>
 </template>
