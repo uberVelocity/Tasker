05175c27283289608ece2fdccb6e9645cd1c429b
ubervelocity <mihaitu.popescu@gmail.com>
2019-09-30 11:56:59 +0200

    restructured

--- tasker/back-end/server/routes/api/about.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-const verify = require('../private/verifyToken');
-
-router.get('/', verify, (req, res) => {
-  res.send(req.user);
-  User.findByOne({_id: req.user});
-});
-
-module.exports = router;
--- tasker/up.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-kubectl apply -f front-end-deployment.yaml
-kubectl apply -f front-end-service.yaml 
-kubectl apply -f ingress.yaml 
-kubectl apply -f back-end-deployment.yaml
-kubectl apply -f back-end-service.yaml 
-
