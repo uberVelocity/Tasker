848aa911f16c446d12d0f10521c17da66ef9bfa8
Julian <julianhegeman@gmail.com>
2019-09-30 12:02:27 +0200

    Updated up.sh and down.sh

--- tasker/kubernetes/down.sh
+++ tasker/kubernetes/down.sh
@@ -5,3 +5,5 @@ kubectl delete service front-end-service
 kubectl delete ingress tasker-ingress 
 kubectl delete deployment back-end-deployment 
 kubectl delete service back-end-service 
+kubectl delete deployment db-deployment 
+kubectl delete service db-service 
--- tasker/kubernetes/up.sh
+++ tasker/kubernetes/up.sh
@@ -4,5 +4,9 @@ kubectl apply -f front-end-deployment.yaml
 kubectl apply -f front-end-service.yaml 
 kubectl apply -f ingress.yaml 
 kubectl apply -f back-end-deployment.yaml
-kubectl apply -f back-end-service.yaml 
+kubectl apply -f back-end-service.yaml
+kubectl apply -f db-service.yaml
+kubectl apply -f db-deployment.yaml 
+
+
 
