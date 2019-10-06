a529aaebf82b97c590006980fab8dd518917ec05
ubervelocity <mihaitu.popescu@gmail.com>
2019-10-06 16:38:09 +0200

    reading values from cassandra

--- co2_sensor/server/index.js
+++ co2_sensor/server/index.js
@@ -1,20 +1,43 @@
 const express = require('express');
 const mongodb = require('mongodb');
+const dotenv = require('dotenv');
+
+const localDatacenter = 'datacenter1';
+const cassandra = require('cassandra-driver');
+const contactPoints = ['172.17.0.3', '172.17.0.3', '172.17.0.3'];
+const loadBalancingPolicy = new cassandra.policies.loadBalancing.DCAwareRoundRobinPolicy(localDatacenter); 
+const clientOptions = {
+   policies : {
+      loadBalancing : loadBalancingPolicy
+   },
+   contactPoints: contactPoints,
+   authProvider: new cassandra.auth.PlainTextAuthProvider('admin', 'q1w2e3r4'),
+   keyspace:'co2'
+};
+const cassandraClient = new cassandra.Client(clientOptions);
 
 const app = express();
+
+dotenv.config();
 
 const port = process.env.PORT || 3001;
 
 // Inserts at every second the value of each server into the database
 async function generateCo2() {
-    console.log(`Server:${Math.random()}, Emissions:${Math.random()}`);
+    const getQuery = 'SELECT * FROM co2consumption';
+    cassandraClient.execute(getQuery, (err, result) => {
+        const firstly = result.first();
+        console.log(`The id of the get is ${firstly.id}. The value: ${firstly.value}.`);
+    });
     const servers = await getServersListFromMongo();
     // For each server in the server list, generate a CO2 emission value and insert it
     // in the database.
     servers.forEach(server => {
         // Use server ID to INSERT value of CO2 and timestamp to corresponding servers
         const serverId = server._id;
+        const co2Value = Math.random();
         console.log(serverId);
+
     });
 }
 
