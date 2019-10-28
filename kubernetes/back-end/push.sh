4bb90ad532a9c76223a5cf85db443017970cf59b
Julian <julianhegeman@gmail.com>
2019-10-28 12:26:54 +0100

    Modified the kubernetes files

--- kubernetes/back-end/push.sh
+++ kubernetes/back-end/push.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 sudo docker login
-sudo docker build -t back-end:latest ../backend
+sudo docker build -t back-end:latest ../tasker/backend
 sudo docker tag back-end:latest s3249212/back-end:latest
 sudo docker push s3249212/back-end:latest
